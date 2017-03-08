b2bc8a1c908d817d99b8a2f747e3a967db2533bf
Matt-Bryan <mbryan_379@yahoo.com>
2017-03-08 10:31:23 -0800

    After lab 3/8

--- DiGraph.java
+++ DiGraph.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 public class DiGraph {
    private LinkedList<Integer>[] graph;
    
-   public DiGraph(int n) {
+   public DiGraph(int n) { //Constructor
       graph = new LinkedList[n];
       for (int i = 0; i < graph.length; i++) {
          graph[i] = new LinkedList<Integer>();
@@ -18,7 +18,7 @@ public class DiGraph {
    
    public void deleteEdge(int from, int to) {
       if (graph[from - 1].contains(to - 1)) {
-         graph[from -1].remove(to - 1);
+         graph[from - 1].remove((Object)(to - 1));
       }
    }
    
@@ -31,20 +31,20 @@ public class DiGraph {
    }
    
    public int vertexCount() {
-      int count = 0;
-      for (int i = 0; i < graph.length; i++) {
-         count += (graph[i].size() + 1);
-      }
-      return count;
+      return graph.length;
    }
    
    public void print() {
       for (int i = 0; i < graph.length; i++) {
          System.out.print((i + 1) + " is connected to: ");
          for (int j = 0; j < graph[i].size(); j++) {
-            System.out.print(graph[i].get(j));
-            if (graph[i].get(j + 1) != null)
+            System.out.print(graph[i].get(j) + 1);
+         	try {
+               graph[i].get(j + 1);
                System.out.print(", ");
+            }
+            catch (IndexOutOfBoundsException e) {
+            }
          }
          System.out.println();
       }
---
+++ DiGraphTest.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class DiGraphTest {
+   
+   public static void main(String[] arg) {
+      Scanner in = new Scanner(System.in);
+      int numVert, exInput1, exInput2;
+      char input;
+      DiGraph graph;
+
+      System.out.print("Enter the number of vertices: ");
+		numVert = in.nextInt();
+
+		graph = new DiGraph(numVert);
+
+		System.out.println("Choose one of the following operations: ");
+		System.out.println("- add edge (enter a)");
+		System.out.println("- delete edge (enter d)");
+		System.out.println("- edge count (enter e)");
+		System.out.println("- vertex count (enter v)");
+		System.out.println("- print graph (enter p)");
+		System.out.println("- Quit (enter q)");
+
+		do {
+			System.out.println("Enter command: ");
+			input = in.next().charAt(0);
+
+			switch (input) {
+				case 'a' :
+				   System.out.println("Enter vertices to connect separated by a space: ");
+			      graph.addEdge((exInput1 = in.nextInt()), (exInput2 = in.nextInt()));
+			      System.out.println("Added edge between " + exInput1 + " and " + exInput2);
+			      break;
+
+			   case 'd' :
+			   	System.out.println("Enter vertices to disconnect separated by a space: ");
+			   	graph.deleteEdge((exInput1 = in.nextInt()), (exInput2 = in.nextInt()));
+			   	System.out.println("Deleted edge between " + exInput1 + " and " + exInput2);
+			   	break;
+
+			   case 'e' :
+			   	System.out.println("Number of edges: " + graph.edgeCount());
+			   	break;
+
+			   case 'v' :
+			   	System.out.println("Number of vertices: " + graph.vertexCount());
+			   	break;
+
+			   case 'p' :
+			   	System.out.println("The graph is the following: ");
+			   	graph.print();
+			   	break;
+
+			   case 'q' :
+			   	System.out.println("Quitting program");
+			   	break;
+
+			   default :
+			   	System.out.println("Invalid command");
+			   	break;
+			}
+		}
+		while (input != 'q');
+   }
+}
